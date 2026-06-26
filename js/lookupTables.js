ce26b80a30210b7c2140e3353bc7b38249f952a7
lhprinco <l.hillermann@gmail.com>
2026-06-26 17:03:59 +0200

    Create lookupTables.js

---
+++ js/lookupTables.js
@@ -0,0 +1,69 @@
+const TYPES=[
+
+"None",
+
+"Normal",
+"Fighting",
+"Flying",
+"Poison",
+"Ground",
+"Rock",
+"Bug",
+"Ghost",
+"Steel",
+"Fire",
+"Water",
+"Grass",
+"Electric",
+"Psychic",
+"Ice",
+"Dragon",
+"Dark",
+"Fairy",
+"Stellar"
+
+];
+
+const ABILITIES=[
+
+"None",
+
+"Water Absorb",
+
+"Flash Fire",
+
+"Adaptability",
+
+"Huge Power",
+
+"Levitate",
+
+"Contrary",
+
+"Battle Armor"
+
+];
+
+const MOVES=[
+
+"None",
+
+"Extreme Speed",
+
+"Boomburst",
+
+"Leaf Blade",
+
+"Giga Drain",
+
+"Close Combat",
+
+"Focus Blast",
+
+"Recover",
+
+"Swords Dance",
+
+"Hydro Steam"
+
+];
